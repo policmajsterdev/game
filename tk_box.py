37b4aea90f78f663389c6b5df5bb42b2455e547b
eimea <polic@majster.hub.pl>
2021-11-11 21:01:44 +0100

    update

--- tk_box.py
+++ tk_box.py
@@ -6,20 +6,24 @@ import db_save
 filepath = os.path.dirname(__file__)
 icon_eq = os.path.join(filepath, "data\\pics\\pleczak.ico")
 tlos = os.path.join(filepath, "data\\pics\\hg.png")
+tloe = os.path.join(filepath, "data\\pics\\hp.png")
 
 
-def informacja(tresc, zrodlo_pliku, wymiary):
+def informacja(tresc, zrodlo_pliku):
 
     eq = "☆ Otrzymujesz " + tresc + " ☆"
     tytul_okna = "Ekwipunek/Statystyki"
     root = Tk()
     root.iconbitmap(icon_eq)
-    root.geometry(wymiary)
+    root.geometry("350x150")
     root.title(tytul_okna)
-    text3_lbl = Label(root, text=eq)
+    tlo = PhotoImage(file=tloe)
+    label_image = Label(root, image=tlo)
+    label_image.place(x=0, y=0, relwidth=1, relheight=1)
+    text3_lbl = Label(root, text=eq, foreground="#FFFFFF", bg="#000000")
     text3_lbl.pack()
     przedmiot = PhotoImage(file=zrodlo_pliku)
-    graph_lbl = Label(root, image=przedmiot)
+    graph_lbl = Label(root, image=przedmiot, relief="flat", bd=0)
     graph_lbl.pack()
     root.mainloop()
 
@@ -29,7 +33,7 @@ def zapiski(osoba, tabela):
     """ Pobiera informacje o osobie """
 
     tytul_okna = "Zapiski na temat " + osoba
-    dane = db_save.pobierz_dane("dane_quest_tomek")
+    dane = db_save.pobierz_dane(tabela)
     root = Tk()
     root.iconbitmap(icon_eq)
     root.geometry("400x400")
@@ -42,6 +46,6 @@ def zapiski(osoba, tabela):
 
     for x in dane:
         tex = x[0] + " : " + x[1]
-        label = Label(root, text=tex, foreground="#FFFFFF", bg="#000000")
+        label = Label(root, text=tex, foreground="#FFFFFF", bg="#000000")  # set your text
         label.pack()
     root.mainloop()
