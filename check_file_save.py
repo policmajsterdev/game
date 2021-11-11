01dcde9a9537f629d2ab1ac5812231c2db1dad13
eimea <polic@majster.hub.pl>
2021-11-11 20:53:49 +0100

    update

--- check_file_save.py
+++ check_file_save.py
@@ -5,9 +5,20 @@ import os.path
 filepath = os.path.dirname(__file__)
 
 
-def checking_files():
+def checking_save():
 
-    """ Sprawdza czy w folderze znajdują się pliki db """
+    """ Sprawdza czy w folderze znajduje się plik save """
+
+    list_file = os.listdir(os.path.join(filepath, "data\\glsav\\"))
+    if len(list_file) == 0:
+        return 0
+    else:
+        return 1
+
+
+def checking_presave():
+
+    """ Sprawdza czy w folderze znajduje się plik pre_save """
 
     list_file = os.listdir(os.path.join(filepath, "data\\save\\"))
     if len(list_file) == 0:
