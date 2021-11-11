51a044daad6931c377a5860de26fc13af93ba86f
Łukasz S <70164924+policmajsterdev@users.noreply.github.com>
2021-11-11 12:22:38 +0100

    Update tk_box.py

--- tk_box.py
+++ tk_box.py
@@ -1,9 +1,11 @@
 import os
 from tkinter import *
+import db_save
 
 
 filepath = os.path.dirname(__file__)
 icon_eq = os.path.join(filepath, "data\\pics\\pleczak.ico")
+tlos = os.path.join(filepath, "data\\pics\\hg.png")
 
 
 def informacja(tresc, zrodlo_pliku, wymiary):
@@ -19,4 +21,27 @@ def informacja(tresc, zrodlo_pliku, wymiary):
     przedmiot = PhotoImage(file=zrodlo_pliku)
     graph_lbl = Label(root, image=przedmiot)
     graph_lbl.pack()
+    root.mainloop()
+
+
+def zapiski(osoba, tabela):
+
+    """ Pobiera informacje o osobie """
+
+    tytul_okna = "Zapiski na temat " + osoba
+    dane = db_save.pobierz_dane("dane_quest_tomek")
+    root = Tk()
+    root.iconbitmap(icon_eq)
+    root.geometry("400x400")
+    root.title(tytul_okna)
+    tlo = PhotoImage(file=tlos)
+    label_image = Label(root, image=tlo)
+    label_image.place(x=0, y=0, relwidth=1, relheight=1)
+    label_head = Label(root, width=0, foreground="#000000", bg="#000000")
+    label_head.pack()
+
+    for x in dane:
+        tex = x[0] + " : " + x[1]
+        label = Label(root, text=tex, foreground="#FFFFFF", bg="#000000")  # set your text
+        label.pack()
     root.mainloop()
