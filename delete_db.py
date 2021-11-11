bd93e54a622b545cd3e0814c74e1a045ddb2b77a
eimea <polic@majster.hub.pl>
2021-11-11 20:58:47 +0100

    update

--- delete_db.py
+++ delete_db.py
@@ -1,5 +1,4 @@
 import os.path
-import db_save
 
 
 filepath = os.path.dirname(__file__)
@@ -7,14 +6,13 @@ filepath = os.path.dirname(__file__)
 
 def delete_db_0_save():
 
-    """ Kasuje plik db gdy gracz wychodzi przed 1 zapisem gry """
+    """ Kasuje pre_save db gdy gracz wychodzi przed zapisem """
+
     try:
         lista_plikow = list(os.listdir(filepath + "\\data\\save"))
         if lista_plikow[0] == "db_account.db":
-            hit = db_save.search_data("scena", "dane_save")
-            if not hit:
-                nowa_sciezka = filepath + "\\data\\save\\" + lista_plikow[0]
-                os.remove(nowa_sciezka)
+            nowa_sciezka = filepath + "\\data\\save\\" + lista_plikow[0]
+            os.remove(nowa_sciezka)
         else:
             pass
     except:
@@ -23,11 +21,14 @@ def delete_db_0_save():
 
 def delete_db_continue():
 
-    """ Kasuje plik db gdy gracz posiada zapis a wybierze nową grę """
+    """ Kasuje save gdy gracz posiada zapis a wybierze nową grę """
 
-    lista_plikow = list(os.listdir(filepath + "\\data\\save"))
-    if lista_plikow[0] == "db_account.db":
-        nowa_sciezka = filepath + "\\data\\save\\" + lista_plikow[0]
-        os.remove(nowa_sciezka)
-    else:
+    try:
+        lista_plikow = list(os.listdir(filepath + "\\data\\glsav"))
+        if lista_plikow[0] == "db_account.db":
+            nowa_sciezka = filepath + "\\data\\glsav\\" + lista_plikow[0]
+            os.remove(nowa_sciezka)
+        else:
+            pass
+    except:
         pass
